d2ec1705f88862ec0b8702962dfcb947058e1123
aipo2 <popo346h@gmail.com>
2016-07-29 23:20:55 +0430

    Update share.lua

--- plugins/share.lua
+++ plugins/share.lua
@@ -2,14 +2,14 @@ do
 
 function run(msg, matches)
 
-if matches[1] == 'share' then
-send_contact(get_receiver(msg), "+6288973997158", "ѕвѕѕ", "Use /Share To Get Number", ok_cb, false)
+if matches[1] == 'شماره' then
+send_contact(get_receiver(msg), "+989335784622", "pouria", "Use /Share To Get Number", ok_cb, false)
 end
 end
 return {
 patterns = {
 "^[#!/]([Ss][Hh][Aa][Rr][Ee])$",
-"^([Ss][Hh][Aa][Rr][Ee])$"
+"^(شماره)$"
 
 },
 run = run
