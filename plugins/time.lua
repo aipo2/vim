0f686d76589a98864fcaf3cc5206654ec6f4be10
aipo2 <popo346h@gmail.com>
2016-07-29 23:02:01 +0430

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -1,108 +1,42 @@
--- Implement a command !time [area] which uses
--- 2 Google APIs to get the desired result:
---  1. Geocoding to get from area to a lat/long pair
---  2. Timezone to get the local time in that lat/long location
-
--- Globals
--- If you have a google api key for the geocoding/timezone api
-api_key  = nil
-
-base_api = "https://maps.googleapis.com/maps/api"
-dateFormat = "%A %d %B - %H:%M:%S"
-
--- Need the utc time for the google api
-function utctime()
-  return os.time(os.date("!*t"))
-end
-
--- Use the geocoding api to get the lattitude and longitude with accuracy specifier
--- CHECKME: this seems to work without a key??
-function get_latlong(area)
-  local api      = base_api .. "/geocode/json?"
-  local parameters = "address=".. (URL.escape(area) or "")
-  if api_key ~= nil then
-    parameters = parameters .. "&key="..api_key
-  end
-
-  -- Do the request
-  local res, code = https.request(api..parameters)
-  if code ~=200 then return nil  end
-  local data = json:decode(res)
- 
-  if (data.status == "ZERO_RESULTS") then
-    return nil
-  end
-  if (data.status == "OK") then
-    -- Get the data
-    lat  = data.results[1].geometry.location.lat
-    lng  = data.results[1].geometry.location.lng
-    acc  = data.results[1].geometry.location_type
-    types= data.results[1].types
-    return lat,lng,acc,types
-  end
-end
-
--- Use timezone api to get the time in the lat,
--- Note: this needs an API key
-function get_time(lat,lng)
-  local api  = base_api .. "/timezone/json?"
-
-  -- Get a timestamp (server time is relevant here)
-  local timestamp = utctime()
-  local parameters = "location=" ..
-    URL.escape(lat) .. "," ..
-    URL.escape(lng) .. 
-    "&timestamp="..URL.escape(timestamp)
-  if api_key ~=nil then
-    parameters = parameters .. "&key="..api_key
-  end
-
-  local res,code = https.request(api..parameters)
-  if code ~= 200 then return nil end
-  local data = json:decode(res)
-  
-  if (data.status == "ZERO_RESULTS") then
-    return nil
-  end
-  if (data.status == "OK") then
-    -- Construct what we want
-    -- The local time in the location is:
-    -- timestamp + rawOffset + dstOffset
-    local localTime = timestamp + data.rawOffset + data.dstOffset
-    return localTime, data.timeZoneId
-  end
-  return localTime
-end
-
-function getformattedLocalTime(area)
-  if area == nil then
-    return "این شهر وجود ندارن"
-  end
-
-  lat,lng,acc = get_latlong(area)
-  if lat == nil and lng == nil then
-    return 'It seems that in "'..area..'" they do not have a concept of time.'
-  end
-  local localTime, timeZoneId = get_time(lat,lng)
-
-  return "زمان در شهر "..timeZoneId.." اینگونه است: ".. os.date(dateFormat,localTime) 
-end
-
+do
 function run(msg, matches)
-  return getformattedLocalTime(matches[1])
-end
+local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then
+ return "No connection"
+  end
+local jdat = json:decode(url)
+local answers = {'https://assets.imgix.net/examples/clouds.jpg?blur=120&w=700&h=400&fit=crop&txt=',
+                   'https://assets.imgix.net/examples/redleaf.jpg?blur=120&w=700&h=400&fit=crop&txt=',
+                   'https://assets.imgix.net/examples/blueberries.jpg?blur=120&w=700&h=400&fit=crop&txt=',
+                   'https://assets.imgix.net/examples/butterfly.jpg?blur=120&w=700&h=400&fit=crop&txt=',
+                   'https://assets.imgix.net/examples/espresso.jpg?blur=120&w=700&h=400&fit=crop&txt=',
+                   'https://assets.imgix.net/unsplash/transport.jpg?blur=120&w=700&h=400&fit=crop&txt=',
+                   'https://assets.imgix.net/unsplash/coffee.JPG?blur=120&w=700&h=400&fit=crop&txt=',
+                   'https://assets.imgix.net/unsplash/citystreet.jpg?blur=120&w=700&h=400&fit=crop&txt=',
+				   'http://assets.imgix.net/examples/vista.png?blur=120&w=700&h=400&fit=crop&txt='}
 
+local fonts = {'American%20Typewriter%2CBold','Arial%2CBoldItalicMT','Arial%2CBoldMT','Athelas%2CBold',
+               'Baskerville%2CBoldItalic','Charter%2CBoldItalic','DIN%20Alternate%2CBold','Gill%20Sans%2CUltraBold',
+			   'PT%20Sans%2CBold','Seravek%2CBoldItalic','Verdana%2CBold','Yuanti%20SC%2CBold','Avenir%20Next%2CBoldItalic',
+			   'Lucida%20Grande%2CBold','American%20Typewriter%20Condensed%2CBold','rial%20Rounded%20MT%2CBold','Chalkboard%20SE%2CBold',
+			   'Courier%20New%2CBoldItalic','Charter%20Black%2CItalic','American%20Typewriter%20Light'}
+
+local colors = {'00FF00','6699FF','CC99CC','CC66FF','0066FF','000000','CC0066','FF33CC','FF0000','FFCCCC','FF66CC','33FF00','FFFFFF','00FF00'}
+local f = fonts[math.random(#fonts)]
+local c = colors[math.random(#colors)]
+
+local url = answers[math.random(#answers)]..jdat.ENtime.."&txtsize=200&txtclr="..c.."&txtalign=middle,center&txtfont="..f.."%20Condensed%20Medium&mono=ff6598cc=?markscale=60&markalign=center%2Cdown&mark64=QFVCX0NICg"
+local file = download_to_file(url,'time.webp')
+send_document(get_receiver(msg) , file, ok_cb, false)
+end
 return {
-  description = "دریافت زمان منطقه", 
-  usage = {
-  "زمان [area]: زمان منطقه جغرافیایی",
-  "time [area]: زمان منطقه جغرافیایی",
-  "/time [area]: زمان منطقه جغرافیایی",
-  },
   patterns = {
-  "^زمان (.*)$",
-  "^time (.*)$",
-  "^[!/#[time] (.*)$"
-  }, 
+     "^([Tt]ime)$",
+	"^([!/#][Tt]ime)$",
+	"^(زمان)$",
+		"^ساعت$",
+  },
   run = run
 }
+
+end
